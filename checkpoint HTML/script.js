f0cb88deae55da1686345b6bdfa6876fe23d3c3b
Saharzitouni <56513755+Saharzitouni@users.noreply.github.com>
2019-12-01 17:59:44 +0100

    Add files via upload

---
+++ checkpoint HTML/script.js
@@ -0,0 +1,89 @@
+// alert("it's just the beginnig ");
+// console.log("it's just the beginnig");
+
+// var x=5;
+// var y=2;
+// var z=x+y;
+// alert(z); 
+
+// var mystring="hello world";
+// alert(mystring.length);
+
+// var firstString="i like";
+// var secondString=" pizza";
+// var completeString=firstString+secondString;
+// alert(completeString);
+
+// var myString="hello there , this is a split test";
+// var parts=myString.split(",");
+// alert(parts);
+
+
+//var i;
+//for (i=0; i<=10; i++)
+//{
+    //alert(i);
+//}
+
+//var i=0;
+//while(i<0){
+  //  alert(i);
+    //i++;
+//}
+
+// var myArray=[1,2,3,4];
+// myArray.push(5);
+// alert(myArray);
+
+// var myArray=[1,2,3,4,5];
+// myArray.pop();
+// alert(myArray);
+var text =document.getElementById("text")
+function bold(){
+  if( text.style.fontWeight=="bold")
+  {
+    text.style.fontWeight="normal"
+  }
+else {
+  text.style.fontWeight="bold"
+}
+}
+
+function italik(){
+  if(text.style.fontStyle=="italic")
+  {
+text.style.fontStyle="normal"
+  }
+  else{
+    text.style.fontStyle="italic"
+  }
+}
+ function underline(){
+   if(text.style.textDecoration=="underline") 
+   {
+     text.style.textDecoration="none";
+   }
+   else{
+     text.style.textDecoration="underline"
+   }
+ }
+
+ function change(){
+   text.style.fontSize= document.getElementById("s").value 
+ }
+ function changer(){
+   text.style.fontFamily=document.getElementById("a").value
+ }
+$("#image1").click(function(){
+  alert("you have clicked on the image")
+});
+
+$(".image").mouseenter(function(){
+  $(this).find(".bt").show();
+  $(this).css("opacity","0.7")
+});
+$(".image").mouseleave(function(){
+  $(this).find(".bt").hide();
+  $(this).css("opacity","1")
+
+});
